157b28e41c594ea72cfae9dad8998cce9012e176
javl <mail@jaspervanloenen.com>
2014-01-20 16:23:18 +0100

    Fixed typo

--- python/serial_test.py
+++ python/serial_test.py
@@ -12,7 +12,7 @@ ser.write('p<100>')
 ser.write('r<100>')
 
 while(1):
-	myInput = raw_input("Enter a number in (0-9) or r<number> / p<number>: ")
+	myInput = raw_input("Enter a number (0-9) or r<number> / p<number>: ")
 	try:
 		ser.write('%d' % int(myInput))
 	except:
